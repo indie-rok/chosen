5c9416cbd82d7787cda8723119f1ff033ecd73f7
Emmanuel Orozco <emmanuel.orozco@kapten.com>
2019-04-09 19:17:46 +0200

    Added facebook login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import firebase from 'react-native-firebase';
-
+import { AccessToken, LoginManager } from 'react-native-fbsdk';
 export default class App extends Component {
   constructor() {
     super();
@@ -19,9 +19,42 @@ export default class App extends Component {
       });
   }
 
+  // Calling the following function will open the FB login dialogue:
+  async facebookLogin() {
+    try {
+      const result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
+
+      if (result.isCancelled) {
+        // handle this however suites the flow of your app
+        throw new Error('User cancelled request');
+      }
+
+      console.log(`Login success with permissions: ${result.grantedPermissions.toString()}`);
+
+      // get the access token
+      const data = await AccessToken.getCurrentAccessToken();
+
+      if (!data) {
+        // handle this however suites the flow of your app
+        throw new Error('Something went wrong obtaining the users access token');
+      }
+
+      // create a new firebase credential with the token
+      const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+
+      // login with credential
+      const firebaseUserCredential = await firebase.auth().signInWithCredential(credential);
+
+      console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   render() {
     return (
       <View>
+        <Button title="Login" onPress={this.facebookLogin} />
         <Text>{this.state.test}</Text>
         <Text>xd5</Text>
       </View>
