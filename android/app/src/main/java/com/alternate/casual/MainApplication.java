729e522e800eb2ecc3c04617654722204eb7744b
Emmanuel Orozco <emmanuel.orozco@kapten.com>
2019-04-09 19:53:30 +0200

    Added push notifications android

--- android/app/src/main/java/com/alternate/casual/MainApplication.java
+++ android/app/src/main/java/com/alternate/casual/MainApplication.java
@@ -3,6 +3,7 @@ package com.alternate.casual;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 //RN Firebase
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new RNFirebasePackage(),
             new RNFirebaseFirestorePackage(),
             new RNFirebaseAuthPackage(),
--- src/App.js
+++ src/App.js
@@ -2,25 +2,11 @@ import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
 import firebase from 'react-native-firebase';
 import { AccessToken, LoginManager } from 'react-native-fbsdk';
+import OneSignal from 'react-native-onesignal';
+
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = { test: `` };
-  }
-
-  componentDidMount() {
-    firebase
-      .firestore()
-      .collection('users')
-      .doc('oneid')
-      .get()
-      .then((snapshot) => {
-        this.setState({ test: snapshot.data().email });
-      });
-  }
-
   // Calling the following function will open the FB login dialogue:
-  async facebookLogin() {
+  static async facebookLogin() {
     try {
       const result = await LoginManager.logInWithReadPermissions(['public_profile', 'email']);
 
@@ -51,12 +37,55 @@ export default class App extends Component {
     }
   }
 
+  constructor() {
+    super();
+    this.state = { test: `` };
+    OneSignal.init('752c15da-c6eb-4f65-9c33-35c7de98d05e');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentDidMount() {
+    console.log(`here`);
+    firebase
+      .firestore()
+      .collection('users')
+      .doc('oneid')
+      .get()
+      .then((snapshot) => {
+        this.setState({ test: snapshot.data().email });
+      });
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   render() {
     return (
       <View>
-        <Button title="Login" onPress={this.facebookLogin} />
+        <Button title="Login" onPress={App.facebookLogin} />
         <Text>{this.state.test}</Text>
-        <Text>xd5</Text>
+        <Text>xd6</Text>
       </View>
     );
   }
