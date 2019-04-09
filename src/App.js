001ca4f79fde03aa3feb06f6f30fa64fa5925203
Emmanuel Orozco <emmanuel.orozco@kapten.com>
2019-04-09 18:11:31 +0200

    Added coverage

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class App extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <View>
-        <Text>Hello</Text>
+        <Text>xd</Text>
       </View>
     );
   }
