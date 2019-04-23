ad8d5c924a3be2b89a93806bbe97269f2b947031
Emmanuel Orozco <emmanuel.orozco@kapten.com>
2019-04-23 17:51:44 +0200

    add native base

--- src/features/login/components/index.js
+++ src/features/login/components/index.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Container, Content, Button, Text } from 'native-base';
 
 export default class Login extends Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text>xd2{Date.now()}</Text>
-      </View>
+      <Container>
+        <Content>
+          <Button>
+            <Text>Connect with Facebook</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
--- src/store.js
+++ src/store.js
@@ -6,15 +6,14 @@ import reducers from './reducers';
 const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers =
+  // eslint-disable-next-line no-underscore-dangle
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    ? // eslint-disable-next-line no-underscore-dangle
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
       })
     : compose;
 
-const enhancer = composeEnhancers(
-  applyMiddleware(sagaMiddleware),
-  // other store enhancers if any
-);
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
 
 export default createStore(reducers, enhancer);
